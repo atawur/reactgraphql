9760b217828d890606add22f16f4ff318a0f2840
Atik <rahmanatawur@gmail.com>
2020-02-13 10:38:04 +0600

    graphql settings

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphQltHttp = require('express-graphql');
+const {buildSchema} = require('graphql');
 const app = express();
 
 app.use(bodyParser.json());
 app.get("/",(req,res,next)=>{
     res.send("hello world");
 })
+app.use('/graphql',graphQltHttp({
+    schema:buildSchema(`
+        type RootQuery{
+            events:[String!]!
+        }
+        type RootMutation{
+            createEvent(name: String): String
+        }
+        schema{
+           query:RootQuery
+           mutation: RootMutation
+        }
+    `),
+    rootValue:{
+        events:()=>{
+            return ['a','b','c'];
+        },
+        createEvent :(arg)=>{
+            const eventName = arg.name;
+            return eventName;
+        },
+        graphiql:true
+
+    }
+}))
 app.listen(3000);
